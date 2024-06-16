90b3871a2ca5e78bf8886ec5fb59d3f0e9390ef6
rolitoxdd <rolo.alv97@gmail.com>
2024-06-16 00:39:21 -0400

    Time: 49 ms (94.46%) | Memory: 51.9 MB (23.37%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter = init
+    return {
+        increment() {
+            return ++counter
+        },
+        decrement() {
+            return --counter
+        },
+        reset() {
+            counter = init
+            return counter
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
