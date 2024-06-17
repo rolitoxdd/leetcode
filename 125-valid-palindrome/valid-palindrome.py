2a87f2b06a47a90f549448f7b1da9fd902e0da9d
rolitoxdd <rolo.alv97@gmail.com>
2024-06-17 13:35:13 -0400

    Time: 54 ms (16.01%) | Memory: 21.7 MB (10.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        letters = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        s = ''.join((c.lower() for c in s if c in letters))
+        print(s)
+        i = 0
+        j = len(s) - 1
+        while i<j:
+            if s[i] != s[j]:
+                return False
+            i+=1
+            j-=1
+        else:
+            return True        
