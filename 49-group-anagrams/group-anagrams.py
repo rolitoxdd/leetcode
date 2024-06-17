45ca882f7b93c9374ecc6fbcc605a1b9bfe971bc
rolitoxdd <rolo.alv97@gmail.com>
2024-06-17 13:20:15 -0400

    Time: 87 ms (68.84%) | Memory: 20.3 MB (51.72%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,16 +4,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
         for w in strs:
-            letter_count = {}
-            for c in w:
-                if c in letter_count:
-                    letter_count[c]+=1
-                else:
-                    letter_count[c] = 1
-            str_items = sorted([f"{item[0]}{item[1]}" for item in letter_count.items()])
-            key = ''.join(str_items)
-            if key in groups:
-                groups[key].append(w)
-            else:
-                groups[key] = [w]
+            sorted_w = ''.join(sorted(w))
+            if sorted_w not in groups:
+                groups[sorted_w] = []
+            groups[sorted_w].append(w)
+
         return groups.values()
