95789a91534820f52666e8882e39ec0ee2ecd982
rolitoxdd <rolo.alv97@gmail.com>
2024-06-22 18:00:04 -0400

    Time: 296 ms (13.21%) | Memory: 26.7 MB (5.92%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,17 +3,14 @@ class Solution:
         left = []
         right = []
         n = len(nums)
+        left_multiplication = 1
+        right_multiplication = 1
         for i in range(len(nums)):
             j = n - 1 - i
-            if i == 0:
-                left_multiplication = 1
-            else:
+            if i != 0:
                 left_multiplication = nums[i-1] * left_multiplication
-            if j == n - 1:
-                right_multiplication = 1
-            else:
+            if j != n - 1:
                 right_multiplication = nums[j+1] * right_multiplication
             left.append(left_multiplication)
             right.append(right_multiplication)
-        res = []
         return [left[i] * right[n-i-1] for i in range(n)]
