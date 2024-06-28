4417bb8c18706446aea5c93c77ae12544711934e
rolitoxdd <rolo.alv97@gmail.com>
2024-06-28 11:50:17 -0400

    Time: 74 ms (75.99%) | Memory: 19.3 MB (63.18%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -16,21 +16,13 @@ class Solution:
             current_res = dummy_head
             while current0 != None and current1 != None:
                 if current0.val < current1.val:
-                    current_res.next = ListNode(current0.val)
+                    current_res.next = current0
                     current0 = current0.next
-                    current_res = current_res.next
                 else:
-                    current_res.next = ListNode(current1.val)
+                    current_res.next = current1
                     current1 = current1.next
-                    current_res = current_res.next
-            while current0 != None:
-                current_res.next = ListNode(current0.val)
-                current0 = current0.next
                 current_res = current_res.next
-            while current1 != None:
-                current_res.next = ListNode(current1.val)
-                current1 = current1.next
-                current_res = current_res.next
+            current_res.next = current0 or current1
             return dummy_head.next
                 
         else:
