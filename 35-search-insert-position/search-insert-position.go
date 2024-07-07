f4476b9f425e38efde35c1b6ce940ec3686d510d
rolitoxdd <rolo.alv97@gmail.com>
2024-07-07 17:34:25 -0400

    Time: 0 ms (100.00%) | Memory: 3.1 MB (72.88%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,8 +1,18 @@
 func searchInsert(nums []int, target int) int {
-    for i, n := range nums {
-        if n >= target {
-            return i
+    low := 0
+    high := len(nums)
+    mid := (low + high) / 2
+    
+    for low < high {
+        if nums[mid] == target {
+            return mid
+        } else if target < nums[mid] {
+            high = mid
+            mid = (low + high) / 2
+        } else {
+            low = mid + 1
+            mid = (low + high) / 2
         }
     }
-    return len(nums)
+    return mid
 }
