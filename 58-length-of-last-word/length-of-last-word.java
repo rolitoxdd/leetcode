1cb8e1b6077504bae2a683eed39df06e1236a22e
rolitoxdd <rolo.alv97@gmail.com>
2024-08-13 07:38:40 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (77.76%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,11 +3,12 @@ class Solution {
         boolean wordFound = false;
         int counter = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
+            if (s.charAt(i) == ' ' && wordFound == true) {
+                break;
+            } else if (s.charAt(i) != ' ') {
                 counter++;
                 wordFound = true;
-            } else if (wordFound == true) {
-                break;
+
             }
         }
         return counter;
