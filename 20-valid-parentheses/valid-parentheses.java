b28c3b6d5a696c318e498f8db2df1667f31f2158
rolitoxdd <rolo.alv97@gmail.com>
2024-09-12 09:25:02 -0300

    Time: 2 ms (52.06%) | Memory: 41.5 MB (53.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,23 +2,23 @@ import java.util.Stack;
         
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> x = new Stack<>();
-        for (int i=0; i<s.length(); i++){
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(' || c == '[' || c == '{') {
-                x.push(c);
-            } else if (x.isEmpty()) {
-                return false; 
-            } else if (c == ')' && x.peek() != '(') {
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
                 return false;
-            } else if (c == ']' && x.peek() != '[') {
+            } else if (c == ']' && stack.peek() != '[') {
                 return false;
-            } else if (c == '}' && x.peek() != '{') {
+            } else if (c == '}' && stack.peek() != '{') {
+                return false;
+            } else if (c==')' && stack.peek() != '(') {
                 return false;
             } else {
-                x.pop();
+                stack.pop();
             }
         }
-        return x.isEmpty();
+        return stack.isEmpty();
     }
 }
