b81065bfc8b6f367c5c259950021294fd0a16ff8
rolitoxdd <rolo.alv97@gmail.com>
2024-12-06 09:32:21 -0300

    Time: 0 ms (100.00%) | Memory: 44.9 MB (57.34%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,27 +1,31 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visited = new boolean[image.length][image[sr].length];
-        return floodFillAux(image, sr, sc, color, visited);
+        boolean[][] visited = new boolean[image.length][image[sc].length];
+        return aux(image, sr, sc, color, visited);
     }
-    public int[][] floodFillAux(int[][] image, int sr, int sc, int color, boolean[][] visited) {
+
+    public int[][] aux(int[][] image, int sr, int sc, int color, boolean[][] visited) {
         if (visited[sr][sc]) return image;
         int originalColor = image[sr][sc];
         image[sr][sc] = color;
         visited[sr][sc] = true;
-
         if (sr - 1 >= 0 && image[sr - 1][sc] == originalColor) {
-            floodFillAux(image, sr - 1, sc, color, visited);
+            // dfs
+            aux(image, sr - 1, sc, color, visited);
         }
         if (sr + 1 < image.length && image[sr + 1][sc] == originalColor) {
-            floodFillAux(image, sr + 1, sc, color, visited);
+            //dfs
+            aux(image, sr + 1, sc, color, visited);
+
         }
-        if (sc - 1 >= 0 && image[sr][sc - 1] == originalColor) {
-            floodFillAux(image, sr, sc - 1, color, visited);
+        if (sc - 1 >= 0 && image[sr][sc-1] == originalColor) {
+            aux(image, sr , sc - 1 , color, visited);
+
         }
-        if (sc + 1 < image[sr].length && image[sr][sc + 1] == originalColor) {
-            floodFillAux(image, sr, sc + 1, color, visited);
+        if (sc + 1 < image[sr].length && image[sr][sc+1] == originalColor) {
+            //dfs
+            aux(image, sr , sc + 1, color, visited);
         }
         return image;
-
     }
 }
