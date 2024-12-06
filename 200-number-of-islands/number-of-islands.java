a2a350b5dbee1670a68b7cc47bea2a64c65d9882
rolitoxdd <rolo.alv97@gmail.com>
2024-12-06 09:13:58 -0300

    Time: 3 ms (86.79%) | Memory: 49.5 MB (56.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,7 +3,8 @@ class Solution {
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+                char cell = grid[i][j];
+                if (cell == '1') {
                     count++;
                     dfs(grid, i, j);
                 }
@@ -12,16 +13,14 @@ class Solution {
         return count;
     }
 
-    void dfs(char[][] grid, int i, int j) {
-        if (i < 0) return;
-        if (j < 0) return;
-        if (i >= grid.length) return;
-        if (j >= grid[0].length) return;
-        if (grid[i][j] != '1') return;
-        grid[i][j] = 'X';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return;
+        char cell = grid[i][j];
+        if (cell == '0' || cell == 'x') return;
+        grid[i][j] = 'x';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1 ,j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 }
