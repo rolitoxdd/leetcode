63695b81d44ce99fe8b6a37f5dac981ed42ed5cb
rolito <rolo.alv97@gmail.com>
2025-07-03 08:58:42 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (18.53%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,14 +1,15 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         records = []
+        res = 0
         for op in operations:
             if op == '+':
                 records.append(records[-1] + records[-2])
             elif op == 'D':
-                records.append(2 * records[-1])
+                records.append(records[-1] * 2)
             elif op == 'C':
                 records.pop()
             else:
                 records.append(int(op))
+
         return sum(records)
-        
